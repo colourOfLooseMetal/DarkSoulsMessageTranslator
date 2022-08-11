32cf71ba0fc870fc13c567aa603f5cab8127026e
Jess <j.martin.peterson@gmail.com>
2022-08-11 15:38:28 -0700

    updated er pug to have elden ring message generator title

--- soapStoneTranslatorJs/er/erpug.js
+++ soapStoneTranslatorJs/er/erpug.js
@@ -7,6 +7,263 @@ Array.prototype.insert = function (index, item) {
   this.splice(index, 0, item);
 };
 
+//img touch canvas from github
+(function() {
+  var root = this; //global object
+
+  var ImgTouchCanvas = function(options) {
+      if( !options || !options.canvas || !options.path) {
+          throw 'ImgZoom constructor: missing arguments canvas or path';
+      }
+
+      this.canvas         = options.canvas;
+      this.canvas.width   = this.canvas.clientWidth;
+      this.canvas.height  = this.canvas.clientHeight;
+      this.context        = this.canvas.getContext('2d');
+
+      this.desktop = options.desktop || false; //non touch events
+      
+      this.position = {
+          x: 0,
+          y: 0
+      };
+      this.scale = {
+          x: 0.5,
+          y: 0.5
+      };
+      this.imgTexture = new Image();
+      this.imgTexture.src = options.path;
+
+      this.lastZoomScale = null;
+      this.lastX = null;
+      this.lastY = null;
+
+      this.mdown = false; //desktop drag
+
+      this.init = false;
+      this.checkRequestAnimationFrame();
+      requestAnimationFrame(this.animate.bind(this));
+
+      this.setEventListeners();
+  };
+
+
+  ImgTouchCanvas.prototype = {
+      animate: function() {
+          //set scale such as image cover all the canvas
+          if(!this.init) {
+              if(this.imgTexture.width) {
+                  var scaleRatio = null;
+                  if(this.canvas.clientWidth > this.canvas.clientHeight) {
+                      scaleRatio = this.canvas.clientWidth / this.imgTexture.width;
+                  }
+                  else {
+                      scaleRatio = this.canvas.clientHeight / this.imgTexture.height;
+                  }
+
+                  this.scale.x = scaleRatio;
+                  this.scale.y = scaleRatio;
+                  this.init = true;
+              }
+          }
+
+          this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+          this.context.drawImage(
+              this.imgTexture, 
+              this.position.x, this.position.y, 
+              this.scale.x * this.imgTexture.width, 
+              this.scale.y * this.imgTexture.height);
+
+          requestAnimationFrame(this.animate.bind(this));
+      },
+
+
+      gesturePinchZoom: function(event) {
+          var zoom = false;
+
+          if( event.targetTouches.length >= 2 ) {
+              var p1 = event.targetTouches[0];
+              var p2 = event.targetTouches[1];
+              var zoomScale = Math.sqrt(Math.pow(p2.pageX - p1.pageX, 2) + Math.pow(p2.pageY - p1.pageY, 2)); //euclidian distance
+
+              if( this.lastZoomScale ) {
+                  zoom = zoomScale - this.lastZoomScale;
+              }
+
+              this.lastZoomScale = zoomScale;
+          }    
+
+          return zoom;
+      },
+
+      doZoom: function(zoom) {
+          if(!zoom) return;
+
+          //new scale
+          var currentScale = this.scale.x;
+          var newScale = this.scale.x + zoom/100;
+          
+
+          //some helpers
+          var deltaScale = newScale - currentScale;
+          var currentWidth    = (this.imgTexture.width * this.scale.x);
+          var currentHeight   = (this.imgTexture.height * this.scale.y);
+          var deltaWidth  = this.imgTexture.width*deltaScale;
+          var deltaHeight = this.imgTexture.height*deltaScale;
+
+
+          //by default scale doesnt change position and only add/remove pixel to right and bottom
+          //so we must move the image to the left to keep the image centered
+          //ex: coefX and coefY = 0.5 when image is centered <=> move image to the left 0.5x pixels added to the right
+          var canvasmiddleX = this.canvas.clientWidth / 2;
+          var canvasmiddleY = this.canvas.clientHeight / 2;
+          var xonmap = (-this.position.x) + canvasmiddleX;
+          var yonmap = (-this.position.y) + canvasmiddleY;
+          var coefX = -xonmap / (currentWidth);
+          var coefY = -yonmap / (currentHeight);
+          var newPosX = this.position.x + deltaWidth*coefX;
+          var newPosY = this.position.y + deltaHeight*coefY;
+
+          //edges cases
+          var newWidth = currentWidth + deltaWidth;
+          var newHeight = currentHeight + deltaHeight;
+          
+          if( newWidth < this.canvas.clientWidth ) return;
+          if( newPosX > 0 ) { newPosX = 0; }
+          if( newPosX + newWidth < this.canvas.clientWidth ) { newPosX = this.canvas.clientWidth - newWidth;}
+          
+          if( newHeight < this.canvas.clientHeight ) return;
+          if( newPosY > 0 ) { newPosY = 0; }
+          if( newPosY + newHeight < this.canvas.clientHeight ) { newPosY = this.canvas.clientHeight - newHeight; }
+
+
+          //finally affectations
+          this.scale.x    = newScale;
+          this.scale.y    = newScale;
+          this.position.x = newPosX;
+          this.position.y = newPosY;
+      },
+
+      doMove: function(relativeX, relativeY) {
+          if(this.lastX && this.lastY) {
+            var deltaX = relativeX - this.lastX;
+            var deltaY = relativeY - this.lastY;
+            var currentWidth = (this.imgTexture.width * this.scale.x);
+            var currentHeight = (this.imgTexture.height * this.scale.y);
+
+            this.position.x += deltaX;
+            this.position.y += deltaY;
+
+
+            //edge cases
+            if( this.position.x > 0 ) {
+              this.position.x = 0;
+            }
+            else if( this.position.x + currentWidth < this.canvas.clientWidth ) {
+              this.position.x = this.canvas.clientWidth - currentWidth;
+            }
+            if( this.position.y > 0 ) {
+              this.position.y = 0;
+            }
+            else if( this.position.y + currentHeight < this.canvas.clientHeight ) {
+              this.position.y = this.canvas.clientHeight - currentHeight;
+            }
+          }
+
+          this.lastX = relativeX;
+          this.lastY = relativeY;
+      },
+
+      setEventListeners: function() {
+          // touch
+          this.canvas.addEventListener('touchstart', function(e) {
+              this.lastX          = null;
+              this.lastY          = null;
+              this.lastZoomScale  = null;
+          }.bind(this));
+
+          this.canvas.addEventListener('touchmove', function(e) {
+              e.preventDefault();
+              
+              if(e.targetTouches.length == 2) { //pinch
+                  this.doZoom(this.gesturePinchZoom(e));
+              }
+              else if(e.targetTouches.length == 1) {
+                  var relativeX = e.targetTouches[0].pageX - this.canvas.getBoundingClientRect().left;
+                  var relativeY = e.targetTouches[0].pageY - this.canvas.getBoundingClientRect().top;                
+                  this.doMove(relativeX, relativeY);
+              }
+          }.bind(this));
+
+          if(this.desktop) {
+              // keyboard+mouse
+              window.addEventListener('keyup', function(e) {
+                  if(e.keyCode == 187 || e.keyCode == 61) { //+
+                      this.doZoom(5);
+                  }
+                  else if(e.keyCode == 54) {//-
+                      this.doZoom(-5);
+                  }
+              }.bind(this));
+
+              window.addEventListener('mousedown', function(e) {
+                  this.mdown = true;
+                  this.lastX = null;
+                  this.lastY = null;
+              }.bind(this));
+
+              window.addEventListener('mouseup', function(e) {
+                  this.mdown = false;
+              }.bind(this));
+
+              window.addEventListener('mousemove', function(e) {
+                  var relativeX = e.pageX - this.canvas.getBoundingClientRect().left;
+                  var relativeY = e.pageY - this.canvas.getBoundingClientRect().top;
+
+                  if(e.target == this.canvas && this.mdown) {
+                      this.doMove(relativeX, relativeY);
+                  }
+
+                  if(relativeX <= 0 || relativeX >= this.canvas.clientWidth || relativeY <= 0 || relativeY >= this.canvas.clientHeight) {
+                      this.mdown = false;
+                  }
+              }.bind(this));
+          }
+      },
+
+      checkRequestAnimationFrame: function() {
+          var lastTime = 0;
+          var vendors = ['ms', 'moz', 'webkit', 'o'];
+          for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+              window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+              window.cancelAnimationFrame = 
+                window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+          }
+
+          if (!window.requestAnimationFrame) {
+              window.requestAnimationFrame = function(callback, element) {
+                  var currTime = new Date().getTime();
+                  var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                  var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+                    timeToCall);
+                  lastTime = currTime + timeToCall;
+                  return id;
+              };
+          }
+
+          if (!window.cancelAnimationFrame) {
+              window.cancelAnimationFrame = function(id) {
+                  clearTimeout(id);
+              };
+          }
+      }
+  };
+
+  root.ImgTouchCanvas = ImgTouchCanvas;
+}).call(this);
+
+
 //we will also scale the image in this function, that is new
 function fit_text_to_rect(text, x1, y1, width, height, imScale, firstRun) {
   var canvas = document.getElementById("canvas"),
@@ -108,6 +365,26 @@ function fit_text_to_rect(text, x1, y1, width, height, imScale, firstRun) {
   // }
   // else {
   lines = get_lines();
+  minTextHeight = 8;
+  // console.log(lines[0].slice(0,11))
+  if(lines[0].slice(0,11) == "<force-font"){
+    // console.log(lines[0].slice(0,16))
+    for(ffl = 0; ffl < Math.min(3,lines.length); ffl += 1){
+    if(lines[ffl].slice(0,16) == "<force-font-all:"){
+      // console.log(lines[ffl].slice(16,18));
+      minTextHeight = parseInt(lines[ffl].slice(16,18));
+      lines.splice(ffl, 1);
+      // console.log(lines)
+    }
+  }
+  //   for(ffl = 0; ffl < Math.min(3,lines.length); ffl += 1){
+  //     console.log(lines[ffl])
+  //     if(lines[ffl].slice(0,12) == "<force-font:"){
+  //       console.log(lines[ffl].slice(12,14));
+  //       // lines.splice(ffl, 1);
+  //     }
+  // }
+  }
   while (1) {
     // if the lines at the current height fit in the meme text space, break
     // console.log("TExt height:", lines.length*textHeight, " height " , height)
@@ -119,7 +396,7 @@ function fit_text_to_rect(text, x1, y1, width, height, imScale, firstRun) {
       textHeight -= 2;
       //lol ok so if we end up smaller than 8, we are going to recursively call this function, but after it runs successfully we will return
       returnPls = false;
-      if (textHeight < 8) {
+      if (textHeight < minTextHeight) {
         //ok so i ran a few large text length trials and found a relationship between the textheight/height at size 8, and the final scale size
         //(textHeight/height)^0.43 gets us close but still below, so lets do ^0.45 on first run then scale up by .1 each time
         if (firstRun) {
@@ -175,14 +452,24 @@ function fit_text_to_rect(text, x1, y1, width, height, imScale, firstRun) {
     1149 * imScale,
     254 * imScale
   );
+  console.log(textHeight)
   //starting y for drawing text is half the text block height above center of y(height) since half below the center half above
   y = height / 2 - Math.round((linesIter / 2) * textHeight);
   for (i = 0; i < lines.length; i++) {
-    ctx.lineWidth = 2 / imScale;
-    ctx.strokeText(lines[i], x1, y1 + y);
-    ctx.fillText(lines[i], x1, y1 + y);
+    tempTxtHeight = textHeight
+    ctx.font = String(tempTxtHeight) + "px GaramondPremierER";
+    if(lines[i].slice(0,16) != "<force-font-all:"){
+      if(lines[i].slice(0,12) == "<force-font:"){
+      tempTxtHeight = parseInt(lines[i].slice(12,14));
+      ctx.font = String(tempTxtHeight) + "px GaramondPremierER";
+      lines[i] = lines[i].slice(14)
+      }
+      ctx.lineWidth = 2 / imScale;
+      ctx.strokeText(lines[i], x1, y1 + y);
+      ctx.fillText(lines[i], x1, y1 + y);
 
-    y += textHeight;
+      y += tempTxtHeight;
+    }
   }
   // }
 }
@@ -645,6 +932,48 @@ for (const [key, value] of Object.entries(wordCategoriesDict)) {
 allWordsSorted = allWordsSorted.sort((a, b) => {
     return a.localeCompare(b, undefined, { sensitivity: 'base' });
 });
+
+// function make_canvas_zoomable(){
+//   var can = document.getElementById("canvas");
+// can.addEventListener("touchstart", ctxTouchStart, false);
+// can.addEventListener("touchmove", ctxTouchMove, false);
+// can.addEventListener("touchend", ctxTouchEnd, false);
+
+// var oX, oY, dX, dY, iX, iY;
+// iX = 0; iY = 0;
+// function ctxTouchStart(e) {
+//     if (!e)
+//         var e = event;
+//     e.preventDefault();
+
+//     mouseIsDown = 0;
+//     oX = e.targetTouches[0].pageX - canvas.offsetLeft;
+//     oY = e.targetTouches[0].pageY - canvas.offsetTop;
+// }
+// function ctxTouchMove(e) {
+//     if (!e)
+//         var e = event;
+//     e.preventDefault();
+//     var canX = e.targetTouches[0].pageX - canvas.offsetLeft;
+//     var canY = e.targetTouches[0].pageY - canvas.offsetTop;
+//     dX = canX - oX;
+//     dY = canY - oY;
+
+//     /*   redraw img on canvas
+//      *   where -(iX + dX) is the startX 
+//      *   and -(iY + dY) is the startY 
+//      */
+//     reDrawImageOnCanvas(can, img, -(iX + dX), -(iY + dY));
+// }
+// function ctxTouchEnd(e) {
+//     if (!e)
+//         var e = event;
+//     e.preventDefault();
+//     iX += dX;
+//     iY += dY;
+// }
+// }
+
 var loadingMessageDotTimeout;
 window.onload = (event) => {
   var updateLoadingMessageCount = 69;
@@ -697,7 +1026,7 @@ function updateLoadingMessage() {
     // console.log(bottomText);
   }
   if(messageType == "custom"){
-    yikes_or_nah.checkEm([topText], handleRes, [topText,messageType],false);
+    yikes_or_nah.checkEm([topText], handleRes, [topText,messageType],false, false);
   }
 
   else if(messageType == "fromTemplates"){
@@ -761,6 +1090,10 @@ function copy_link() {
   dummy.select();
   document.execCommand('copy');
   document.body.removeChild(dummy);
+  document.getElementById("loading").innerHTML = "link copied!";
+  document.getElementById("loading").style.display = "block";
+  setTimeout(function(){ document.getElementById("loading").style.display = "none"; }, 3000);
+  
 }
 
 function make_meme(topText, bottomText, messageType) {
